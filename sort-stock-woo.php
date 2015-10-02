7277d14505a52c3ba8ff0dac877a0c9011279f1c
Elton Pereira <eltondeveloper@gmail.com>
2015-10-02 15:12:24 -0300

    Update sort-stock-woo.php

--- sort-stock-woo.php
+++ sort-stock-woo.php
@@ -68,3 +68,26 @@ function manage_wp_posts_clauses( $stock_order, $query ) {
     }
     return $stock_order;
 }
+
+//Starts ordination in WooCommerce catalog
+function custom_woocommerce_get_catalog_ordering_args( $args ) {
+  $orderby_value = isset( $_GET['orderby'] ) ? woocommerce_clean( $_GET['orderby'] ) : apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby' ) );
+
+	if ( 'stock' == $orderby_value ) {
+		$args['orderby'] = 'meta_value';
+		$args['order'] = 'asc';
+		$args['meta_key'] = '_stock_status';
+	}
+
+	return $args;
+}
+
+add_filter( 'woocommerce_default_catalog_orderby_options', 'custom_woocommerce_catalog_orderby' );
+add_filter( 'woocommerce_catalog_orderby', 'custom_woocommerce_catalog_orderby' );
+
+function custom_woocommerce_catalog_orderby( $sortby ) {
+	$sortby['stock'] = 'Estoque Status';
+	return $sortby;
+}
+
+add_filter( 'woocommerce_get_catalog_ordering_args', 'custom_woocommerce_get_catalog_ordering_args' );
